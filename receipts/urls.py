62b3da4b91eb6d8ec0170de4f1f46f5f30b10264
Mac <macstephens@gmail.com>
2023-04-19 13:12:04 -0400

    Feature 6 Complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from receipts.views import get_receipt
 
 urlpatterns = [
-    path("", get_receipt, name="get_receipt_home"),
+    path("", get_receipt, name="home"),
 ]
